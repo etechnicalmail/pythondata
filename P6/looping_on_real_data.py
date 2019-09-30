7df8f1e16b3f19c8628735e43dc21cafa5825694
eknath <1nath.salvi@gmail.com>
2019-09-30 08:46:36 +0530

    123

---
+++ P6/looping_on_real_data.py
@@ -0,0 +1,22 @@
+personDetails = [
+            {'name': 'John', 'age': 15, 
+             'cars': [
+                 {'name': 'Ford', 'models': ['Fiesta', 'Focus', 'Mustang']},
+                 {'name': 'Maruti', 'models': ['800', 'Suzuki']},
+                 {'name': 'Fiat', 'models': ['500', 'Panda']}]},
+            {'name': 'chandu', 'age': 23,
+             'cars': [
+                 {'name': 'Tata', 'models': ['Nano', 'Manza']}, 
+                 {'name': 'BMW', 'models': ['320', 'X3', 'X5']},
+                 {'name': 'Ferrai', 'models': ['500', 'Panda']}]},
+            {'name': 'ashok', 'age': 25,
+             'cars': [
+                 {'name': 'Maruti', 'models': ['1200', 'Suzuki']}, 
+                 {'name': 'BMW', 'models': ['320', 'X3', 'X5']},
+                 {'name': 'Fiat', 'models': ['500', 'Panda']}]},
+            {'name': 'akshay', 'age': 30, 
+             'cars': [
+                 {'name': 'Ford', 'models': ['Fiesta', 'Focus', 'Mustang']},
+                 {'name': 'BMW', 'models': ['320', 'X3', 'X5']},
+                {'name': 'Maruti', 'models': ['800', 'Suzuki']}]}
+            ] 
