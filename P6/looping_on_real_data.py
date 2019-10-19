f496e5358db033c1b7e56175c45273849cf65422
eknath <1nath.salvi@gmail.com>
2019-10-19 10:11:47 +0530

    flask2

--- P6/looping_on_real_data.py
+++ P6/looping_on_real_data.py
@@ -16,21 +16,43 @@ personDetails = [
                  {'name': 'Fiat', 'models': ['500', 'Panda']}]},
             {'name': 'akshay', 'age': 30, 
              'cars': [
-                 {'name': 'Ford', 'models': ['Fiesta', 'Focus', 'Mustang']},
+                 {'name': 'Ford', 'models': ['fiesta', 'focus', 'Mustang']},
                  {'name': 'BMW', 'models': ['320', 'X3', 'X5']},
                 {'name': 'Maruti', 'models': ['800', 'Suzuki']}]}
             ]
 result = 0
 names = list()
+'''
 for person in personDetails:
     for car in person['cars']:
-        if car['name']=='Fiat' and ('Panda' in car['models']):
+        if car['name']=='BMW' and ('320' in car['models']):
             print(person['name'])
             result = result + 1
             names.append(person['name'])
 
 print(names)
 print(result)
+
+
+for person in personDetails:
+    for car in person['cars']:
+        for model in car['models']:
+            if 'f' in model.lower():
+                res_string = f"person: {person['name']} age: {person['age']} car name: {car['name']}" 
+                print(res_string)
+                names.append(res_string)
+
+print(names)
+
+results = dict()
+for person in personDetails:
+    model_count = 0
+    for car in person['cars']:
+        model_count = model_count + len(car['models'])
+        # print(f"person: {person['name']} car: {car['name']} car models: {', '.join(car['models'])}")
+    results[person['name']] = model_count
+print(results)
+'''
     
 
 
